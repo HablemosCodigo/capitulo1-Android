e9a4595a4ae2e4f1f2ece3f4e38ab4e053c7d186
adan De santiago <adan1995a@gmail.com>
2016-11-13 19:38:41 -0600

    agrege boton con listener

--- app/src/main/java/com/example/adn/androidcapitulo1/MainActivity.java
+++ app/src/main/java/com/example/adn/androidcapitulo1/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.adn.androidcapitulo1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    Button boton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        boton=(Button)findViewById(R.id.boton1);
+        boton.setOnClickListener(this);
     }
+    public void onClick(View v){
+        if(v.getId()==R.id.boton1){
+
+            
+        }
+    }
+
 }
+
